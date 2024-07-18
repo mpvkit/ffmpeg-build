ad1e55f9fb63d73845c4f9cd173f22c2dc5178b1
cxfksword <718792+cxfksword@users.noreply.github.com>
2024-07-19 01:06:08 +0800

    limit meson version to 1.4.2

--- Sources/BuildScripts/XCFrameworkBuild/base.swift
+++ Sources/BuildScripts/XCFrameworkBuild/base.swift
@@ -1077,6 +1077,11 @@ enum Utility {
         }
         if !environment.keys.contains("PATH") {
             environment["PATH"] = BaseBuild.defaultPath
+
+            // meson need to use pip version on GITHUB ACTION, use brew version will build failed
+            if ProcessInfo.processInfo.environment.keys.contains("GITHUB_ACTION") {
+                environment["PATH"] = "/Library/Frameworks/Python.framework/Versions/Current/bin:" + environment["PATH"]!
+            }
         }
         task.environment = environment
 
@@ -1156,11 +1161,24 @@ enum Utility {
             if let logURL = logURL {
                 // print log when run in GitHub Action
                 if ProcessInfo.processInfo.environment.keys.contains("GITHUB_ACTION") {
-                    print("############# \(logURL) CONTENT BEGIN #############")
                     if let content = String(data: try Data(contentsOf: logURL), encoding: .utf8) {
+                        print("############# \(logURL) CONTENT BEGIN #############")
                         print(content)
+                        print("#############  \(logURL) CONTENT END #############")
+                        if #available(macOS 13.0, *) {
+                            let regErrLogPath = try Regex("A full log can be found at\\s+?(/.*\\.txt)")
+                            if let firstMatch = content.firstMatch(of: regErrLogPath) {
+                                let errPath = "\(firstMatch[1].value ?? "")"
+                                if !errPath.isEmpty {
+                                    print("############# \(errPath) CONTENT BEGIN #############")
+                                    let content = Utility.shell("cat \(errPath)", isOutput: true)
+                                    print(content ?? "")
+                                    print("#############  \(errPath) CONTENT END #############")
+                                }
+                            }
+                        }
                     }
-                    print("#############  \(logURL) CONTENT END #############")
+                    
                 }
                 print("please view log file for detail: \(logURL)\n")
             }
