68600d4d872f191970d1d2177639fedb54756bfc
cxfksword <718792+cxfksword@users.noreply.github.com>
2024-08-14 09:10:45 +0800

    chore: update libshaderc bundle identifier

--- Sources/BuildScripts/XCFrameworkBuild/main.swift
+++ Sources/BuildScripts/XCFrameworkBuild/main.swift
@@ -71,7 +71,7 @@ enum Library: String, CaseIterable {
         case .vulkan:
             return "1.2.9"
         case .libshaderc:  // compiling GLSL (OpenGL Shading Language) shaders into SPIR-V (Standard Portable Intermediate Representation - Vulkan) code
-            return "2024.1.0"
+            return "2024.1.0-fix"
         }
     }
 
